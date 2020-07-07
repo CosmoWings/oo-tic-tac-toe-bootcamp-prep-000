d33574b43d8e15b0d3a376fc9f17b32b34753a95
Patrick Pierre <patrick.pierre000@gmail.com>
2020-07-07 20:19:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,7 +95,7 @@ class TicTacToe
   def won?
     WIN_COMBINATIONS.detect do |combo|
       @board[combo[0]] == @board[combo[1]] &&
-      @board[combo[1]] == @board[combo[2]]
+      @board[combo[1]] == @board[combo[2]] && @board[combo[0]] != " "
     end
   end
 
